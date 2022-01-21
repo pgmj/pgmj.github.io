2ea26ae8aa39f54ee25a4c439ef44191a232e720
Magnus Johansson <31865392+pgmj@users.noreply.github.com>
2022-01-21 19:22:50 +0100

    updates to local deps and unidimensionality

--- raschRcomparisons.R
+++ raschRcomparisons.R
@@ -28,7 +28,7 @@ df[,1:7]<-NULL # remove GNI toxic subscale items
 df<-na.omit(df)
 # WAAQ has N = 230, GNItoxic has 582
 
-# TAM needs 0 as the base response category, and eRm too, mirt?
+# some programs (TAM, eRm) needs 0 as the base response category. mirt?
 library(car)
 for (i in 1:ncol(df)) {
   df[,i]<-recode(df[,i],"1=0;2=1;3=2;4=3;5=4;6=5;7=6",as.factor=F) # adjust number of response options depending on data
@@ -51,11 +51,6 @@ mirtall<-cbind(mitemfit[,2:5],mitemlocation)
 mirtall
 
 library(TAM)
-# TAM needs 0 as the base response category
-library(car)
-for (i in 1:ncol(df)) {
-  df[,i]<-recode(df[,i],"1=0;2=1;3=2;4=3;5=4;6=5;7=6",as.factor=F) # adjust number of response options depending on data
-}
 df.m<-as.matrix(df)
 df.tam <- tam(df.m, irtmodel = "PCM")
 IRT.WrightMap(df.tam,show.thr.lab=TRUE)
@@ -93,10 +88,6 @@ item.fit # view output
 item.fit.table <- cbind(item.fit[["i.outfitMSQ"]],item.fit[["i.infitMSQ"]],item.fit[["i.infitMSQ"]],item.fit[["i.infitZ"]])
 pfit <- personfit(person.locations.estimate)
 person.fit.table <- cbind(pfit[["p.outfitMSQ"]],pfit[["p.outfitMSQ"]],pfit[["p.outfitMSQ"]],pfit[["p.outfitMSQ"]])
-
-
-### lordif
-library(lordif)
 
 
 ### import Winsteps data
@@ -141,16 +132,107 @@ gender<-df.gni$Sex
 
 # Run Age-related DIF
 ageDIF <- lordif(df, gender, criterion="Chisqr", alpha=0.01, minCell=5, model = "GPCM",
-                 MonteCarlo = T, nr = 200)
+                 MonteCarlo = T, nr = 1000)
 #print(ageDIF)
 #summary(ageDIF)
 plot(ageDIF, labels=c("Male", "Female"))
+
+# DIF with eRm:
+# https://bookdown.org/chua/new_rasch_demo2/DIF.html#r-lab-2-dif-analysis-with-the-partial-credit-model
+subgroup_diffs <- Waldtest(df.erm, splitcr = gender)
+
+
+# TAM: create EVC's separately for gender DIF visual analysis ?
+
+df.sex<-cbind(df,gender)
+names(df.sex)
+df.male<-subset(df, gender == 1)
+df.female<-subset(df, gender == 2)
+df.male$gender <- NULL
+df.female$gender <- NULL
+df.male<-as.matrix(df.male)
+df.female<-as.matrix(df.female)
+df.tam.m <- tam(df.male, irtmodel = "PCM")
+df.tam.f <- tam(df.female, irtmodel = "PCM")
+plot(df.tam, item = 3)
+plot(df.tam.m, item = 3)
+plot(df.tam.f, item = 3)
+# changes slope of curve, making it difficult to compare...
+
+
+# https://rdrr.io/github/cswells1/MeasInv/ maybe offers a useful option?
+#install.packages("devtools")
+#devtools::install_github("cswells1/MeasInv") 
+library("MeasInv")
+df.sex$gender<-factor(df.sex$gender)
+df.sex$gender<-car::recode(df.sex$gender,"1=0;2=1")
+# Perform Mantel DIF procedure for polytomous item responses #
+Mantel.output <- Mantel.poly(data = df.sex[,1:7], group = df.sex$gender,
+                             focal.name = 2, ref.name = 1, sig.level = .01, purify = TRUE)
+Mantel.output
+# Perform GMH DIF procedure for polytomous item responses #
+GMH.results <- GMH.poly(data = Likert.data[,1:12], group = Likert.data$group, 
+                        sig.level = .05, purify = TRUE)
 
 # Run Monte Carlo simulation to generate cut-off thresholds
 #ageMC <- montecarlo(ageDIF, alpha=.01, nr=100) #this may take about an hour
 #print(ageMC)
 #summary(ageMC)
 #plot(ageMC)
+
+
+### local dependencies, residual correlations
+# mirt
+resid=residuals(mirt.rasch, type="Q3", digits=2)
+diag(resid) <- NA # make the diagonal of correlation matrix NA instead of 1
+# use function from ltm package to check for local deps, with p-value adjustment for multiple analysis
+library("ltm")
+rcor.test(resid, p.adjust = T) # only provides absolute correlations
+# if significant correlations exist, check size compared to average:
+mean(resid, na.rm=T) # get the mean residual value, ignoring NA
+mirt.resid.limit<- mean(resid, na.rm=T) + 0.25 # create variable indicating dynamic cutoff
+mirt.resid.limit #check value
+Filter(function(x) any(x > mirt.resid.limit), resid) #identify values above cutoff, then find them in matrix
+mirt.residualmatrix<-round(resid, 2) #store the corr matrix
+mirt.residualmatrix
+
+
+### unidimensionality check
+# mokken scaling
+library(mokken)
+coefH(df) # H should be above 0.3
+aisp(df) # should be 1
+
+# CFA
+library(lavaan)
+cfa.model <- 'factor1 =~ GNI_1+GNI_2+GNI_3+GNI_4+GNI_5+GNI_6+GNI_21'
+cfa.fit.dwls <- cfa(cfa.model, data=df, ordered=names(df)) # DWLS for ordinal data
+summary(cfa.fit.dwls, fit.measures=T, standardized=T)
+#cfa.fit.ml <- cfa(cfa.model, data = df) # ML for fun comparison
+#summary(cfa.fit.ml, fit.measures=T, standardized=T)
+
+# PCA of residuals, guided by https://bookdown.org/chua/new_rasch_demo2/MD-fit.html#conduct-a-pca-of-standardized-residual-correlations
+# based on eRm residuals
+library(psych)
+std.resids <- item.fit$st.res
+pca <- pca(std.resids, nfactors = ncol(df), rotate = "none")
+pca$values # check eigenvalues, should be below 2.0
+# plot if desired
+contrasts <- c(pca$values[1], pca$values[2], pca$values[3], pca$values[4], pca$values[5])
+plot(contrasts, ylab = "Eigenvalues for Contrasts", xlab = "Contrast Number", main = "Contrasts from PCA of Standardized Residual Correlations")
+
+
+# response category tresholds
+# mirt plot:
+plot(mirt.rasch, type="trace", xlim = c(-6,6))
+# TAM plot:
+plot(df.tam, type = "items")
+# add code to generate numerical comparisons for thresholds, scaled to center at 0
+
+
+# person location comparisons
+
+
 
 ### ltm estimation; "approximate marginal Maximum Likelihood, using the 
 # Gauss-Hermite quadrature rule for the approximation of the required integrals"
